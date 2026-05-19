ae36450a7b46781fe6493e359b755a1297fa094d
Harshil Chauhan <harshilchauhan3617u@gmail.com>
2026-05-19 17:07:29 +0530

    Updates

--- assets/Navbar-UmxaOVtG.js
+++ assets/Navbar-UmxaOVtG.js
@@ -1,4 +1,4 @@
-import{j,r as bt}from"./index-R5_d-3Nn.js";import{g as St,S as _t}from"./ScrollTrigger-0lJgtTtq.js";/*!
+import{j,r as bt}from"./index-koagG3u7.js";import{g as St,S as _t}from"./ScrollTrigger-0lJgtTtq.js";/*!
  * SplitText 3.14.2
  * https://gsap.com
  *
